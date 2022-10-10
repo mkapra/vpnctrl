044eb9932f4f906ee0135b3ff0d2602c103a005e
Maximilian Kapra <maximilian@kapra.de>
2022-10-10 16:09:26 +0200

    Add configuration method for client

--- libwgbuilder/src/models/client.rs
+++ libwgbuilder/src/models/client.rs
@@ -3,12 +3,14 @@
 //! A client is a device in the VPN network.
 use anyhow::Result;
 use diesel::{
-    Associations, Identifiable, Insertable, PgConnection, QueryDsl, Queryable, RunQueryDsl,
+    prelude::*, Associations, Identifiable, Insertable, PgConnection, QueryDsl, Queryable,
+    RunQueryDsl,
 };
+use sailfish::TemplateOnce;
 
-use crate::{schema::clients, Error};
+use crate::{models::Server, schema::clients, Error};
 
-use super::{DnsServer, Keypair, Model, VpnIp};
+use super::{DnsServer, Keypair, Model, VpnIp, VpnNetwork};
 
 /// Client from the database
 #[derive(Identifiable, Queryable, Associations)]
@@ -35,6 +37,66 @@ impl Model for Client {
             anyhow::Error::from(e).context(Error::DatabaseObjectNotFound("client", search_id))
         })
     }
+}
+
+impl Client {
+    /// Returns a wireguard configuration for the client
+    ///
+    /// # Returns
+    /// If the configuration has missing parts (e.g. no server in VPN network) an `Err` will be returned
+    pub fn configuration(&self, conn: &mut PgConnection) -> Result<String> {
+        let keypair = Keypair::find(self.keypair_id, conn)?;
+        let vpn_ip = VpnIp::find(self.vpn_ip_id, conn)?;
+        let vpn_network = VpnNetwork::find(vpn_ip.vpn_network_id, conn)?;
+        let dns_server = DnsServer::find(self.dns_server_id, conn)?;
+
+        // Find the corresponding server in the VPN network of the client
+        use crate::schema::vpn_ips::{self, dsl::*};
+        let ids_ip_network = vpn_ips::table
+            .filter(vpn_network_id.eq(vpn_network.id))
+            .load::<VpnIp>(conn)?
+            .iter()
+            .map(|e| e.id)
+            .collect::<Vec<i32>>();
+        use crate::schema::servers::{self, dsl::*};
+        let (server, _): (Server, VpnIp) = servers::table
+            .inner_join(vpn_ips::table)
+            .filter(vpn_ip_id.eq_any(ids_ip_network))
+            .first(conn)
+            .map_err(|e| {
+                anyhow::Error::from(e)
+                    .context(Error::Database)
+                    .context("Could not get server from database")
+            })?;
+        let server_keypair = Keypair::find(server.keypair_id, conn)?;
+
+        let ctx = ClientConfig {
+            private_key: keypair.private_key,
+            ip_address: vpn_ip.address,
+            netmask: vpn_network.subnetmask,
+            dns_server: dns_server.ip,
+            peer_public_key: server_keypair.public_key,
+            endpoint_address: server.external_ip,
+            endpoint_port: vpn_network.port,
+            keepalive: self.keepalive,
+        };
+        ctx.render_once().map_err(|e| {
+            anyhow::Error::from(e).context("Could not create configuration for client")
+        })
+    }
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "client_configuration.stpl")]
+struct ClientConfig {
+    private_key: String,
+    ip_address: String,
+    netmask: i32,
+    dns_server: String,
+    peer_public_key: String,
+    endpoint_address: String,
+    endpoint_port: i32,
+    keepalive: i32,
 }
 
 /// Client that is not created in the database yet
