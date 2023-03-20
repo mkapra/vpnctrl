e3e0cf80f1922b2b6888b0a3c58e5e10cbb2c40e
Maximilian Kapra <maximilian@kapra.de>
2023-03-20 22:53:59 +0100

    [CLI] Initial commit

---
+++ cli/src/commands/mod.rs
@@ -0,0 +1,61 @@
+use graphql_client::reqwest::post_graphql_blocking as post_graphql;
+use inquire::{Text, required, Password};
+use reqwest::blocking::Client;
+
+use crate::{State, COMMANDS, queries::{Login, login, NewClientInformation}};
+
+pub fn print_help(_args: Vec<String>, ctx: State) -> State {
+    for cmd in COMMANDS.iter() {
+        println!("{}:\t{}", cmd.name, cmd.help);
+    }
+    ctx
+}
+
+pub fn login(_args: Vec<String>, ctx: State) -> State {
+    let api_url = Text::new("API URL:")
+        .prompt()
+        .unwrap();
+    let username = Text::new("Username:")
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
+    let password = Password::new("Password:")
+        .without_confirmation()
+        .with_validator(required!())
+        .prompt()
+        .unwrap();
+
+    let client = Client::builder()
+        .default_headers(
+            std::iter::once((
+                reqwest::header::AUTHORIZATION,
+                reqwest::header::HeaderValue::from_str("Bearer YESYOUFOUNDABUG").unwrap()
+            )).collect()
+        )
+        .build()
+        .unwrap();
+
+    let variables = login::Variables {
+        username,
+        password
+    };
+
+    let mut state = State {
+        ..ctx
+    };
+    if !api_url.is_empty() {
+        state.url = api_url;
+    }
+
+    let res = post_graphql::<Login, _>(&client, &state.url, variables).unwrap();
+    let res_data = res.data.expect("Missing response data");
+    state.jwt_token = res_data.login;
+
+    println!("State: {:?}", state);
+    return state;
+}
+
+pub fn new_client(_args: Vec<String>, ctx: State) -> State {
+    NewClientInformation::create_client(&ctx).unwrap();
+    ctx
+}
---
+++ cli/src/main.rs
@@ -0,0 +1,67 @@
+use inquire::{required, Text};
+use once_cell::sync::Lazy;
+
+mod commands;
+use commands::*;
+mod queries;
+
+#[derive(Debug)]
+pub struct State {
+    pub url: String,
+    pub jwt_token: String,
+}
+
+impl State {
+    fn new(url: Option<&str>) -> Self {
+        Self {
+            jwt_token: "".to_string(),
+            url: url.map(|u| u.to_string()).unwrap_or_else(|| "http://localhost:3000".to_string())
+        }
+    }
+}
+
+static COMMANDS: Lazy<Vec<Command<fn(Vec<String>, State) -> State>>> = Lazy::new(|| {
+    vec![
+        Command::new("help", "Prints out this help", print_help),
+        Command::new("login", "Is required to retrieve a token from the API", login),
+        Command::new("new_client", "Creates a new client in the network", new_client),
+    ]
+});
+
+struct Command<C>
+where
+    C: Fn(Vec<String>, State) -> State,
+{
+    name: String,
+    exec: C,
+    help: String,
+}
+
+impl<C> Command<C>
+where
+    C: Fn(Vec<String>, State) -> State,
+{
+    fn new(name: &str, help: &str, exec: C) -> Self {
+        Self {
+            name: name.to_string(),
+            help: help.to_string(),
+            exec,
+        }
+    }
+}
+
+fn main() {
+    let mut state = State::new(None);
+    loop {
+        let cmd = Text::new("> ")
+            .with_validator(required!())
+            .prompt()
+            .unwrap();
+        for existing_cmd in COMMANDS.iter() {
+            if existing_cmd.name == cmd {
+                state = (existing_cmd.exec)(vec!["".to_string()], state);
+                break;
+            }
+        }
+    }
+}
